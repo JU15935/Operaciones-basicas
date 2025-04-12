c3abeeff9c52b2c4349d04e7b3164b3eb49946ce
JU15935 <sanchezchaverrajuanmiguel@gmail.com>
2025-04-12 15:37:28 -0500

    Main2.java

---
+++ Main2.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+public class Operacionesbasicas {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner (System.in);
+    //Solicitar nombre
+        System.out.print("Por favor, ingrese el nombre:");
+      String nombre = scanner.nextLine();
+      // solicitar edad
+         System.out.print("Por favor, ingrese la edad:");
+           String edad = scanner.nextLine();
+           // solicitar correo
+             System.out.print("Por favor, ingrese el correo:");
+                 String correo = scanner.nextLine();
+         
+          scanner.nextLine();
+        
+          System.out.println ("numero de datos ingresados");
+          System.out.println ("nombre:" + nombre);
+          System.out.println ("edad:" + edad);
+          System.out.println ("correo:" + correo);
+    }
+}import java.util.Scanner;
+public class Operacionesbasicas {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner (System.in);
+    //Solicitar nombre
+        System.out.print("Por favor, ingrese el nombre:");
+      String nombre = scanner.nextLine();
+      // solicitar edad
+         System.out.print("Por favor, ingrese la edad:");
+           String edad = scanner.nextLine();
+           // solicitar correo
+             System.out.print("Por favor, ingrese el correo:");
+                 String correo = scanner.nextLine();
+         
+          scanner.nextLine();
+        
+          System.out.println ("numero de datos ingresados");
+          System.out.println ("nombre:" + nombre);
+          System.out.println ("edad:" + edad);
+          System.out.println ("correo:" + correo);
+    }
+}
